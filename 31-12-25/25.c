76880de0bb1470962ddcf58ee059500e74baf5d0
pavankakarla-912 <pavankakarla912@gmail.com>
2025-12-31 19:53:20 +0530

    Add files via upload

---
+++ 31-12-25/25.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    original = num;
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+    if (original == reversed)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+    return 0;
+}
