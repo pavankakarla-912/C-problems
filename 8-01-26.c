c912c2871076298c6c103a14f8ea5fd70b494e60
pavankakarla-912 <pavankakarla912@gmail.com>
2026-01-08 20:21:28 +0530

    Add files via upload

---
+++ 8-01-26.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){ // 9 1 2 3 0
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+}
